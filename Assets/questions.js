50c51868bc723229157cfb9276d145c31de8be40
Armando Herrera Canelon <armando.herrera91@gmail.com>
2019-12-31 15:47:55 -0500

    Adding the rest of the questions

--- Assets/questions.js
+++ Assets/questions.js
@@ -8,5 +8,21 @@ var questions = [
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can beused to store ____.",
+        choices: ["numbers and strings" , "other arrays" , "booleans" , "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: " A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [" quotes ", " terminal / bash " , " for loops " , "console.log"],
+        answer: "console.log"
+    },
+    {
+        title: "Strings values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas" , "curly brackets" , "quotes" , "parentheses"],
+        answer: "curly brackets"
     }
+
 ];
